d5d28a4b35a88a2161abc2fdaad44e4138684637
Gumirov Andrey <andreyt45@gmail.com>
2020-09-10 22:10:27 +0700

    Completed task #2

--- task-1/main.cpp
+++ task-1/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include <iomanip>
+#include <math.h>
+
 #define ABS(x) ((x)>=0 ? (x):(-(x)))
 
 using namespace std;
 
 struct complex { // a + ib
-    int a;
-    int b;
+    double a;
+    double b;
 };
 
-int main(){
+void print_complex(complex z){
+    cout << setprecision(12) << z.a << " " << (z.b>=0?"+ ":"- ") << "i*" << setprecision(12) << ABS(z.b);
+}
+
+void print_complex_trig(complex z){
+    double phi = atan(z.b/z.a);
+    cout << setprecision(12) << sqrt(z.a*z.a + z.b*z.b) << " * (cos(" << setprecision(12) << phi << ") + i*sin(" << setprecision(12) << phi << "))";
+}
+
+int main(int argc, char * argv[]){
+    // if (argc < 2)
     complex z = complex{};
     cout << "Please, input a and b for z = a + ib:" << endl;
     cin >> z.a >> z.b;
@@ -16,21 +29,37 @@ int main(){
     int choice = -1;
     cin >> choice;
     switch (choice){
-        case 0:
-            cout << "Trigonometric number notation: " << z.a << " " << (z.b>=0?"+ ":"- ") << "i" << ABS(z.b) << endl;
+        case 0: { // just print out numbers
+            print_complex_trig(z);
+            cout << endl;
             break;
-        case 1:
+        }
+        case 1: { // print z^-1
+            complex z_inverse = complex{};
+            
+            z_inverse.a = z.a/(z.a*z.a+z.b*z.b);
+            z_inverse.b = -z.b/(z.a*z.a+z.b*z.b);
+
+            cout << "z^-1: " ;
+            print_complex(z_inverse);
+            cout << endl;
+            cout << "Trig: ";
+            print_complex_trig(z_inverse);
+            cout << endl;
+            break;
+        }
+        case 2: { // print z^n
             cout << "Unimplemented!" << endl;
             break;
-        case 2:
+        }
+        case 3: {
             cout << "Unimplemented!" << endl;
             break;
-        case 3:
-            cout << "Unimplemented!" << endl;
-            break;
-        default:
+        }
+        default: {
             cout << "Wrong choice!" << endl;
             return -1;
+        }
     }
     return 0;
 }
